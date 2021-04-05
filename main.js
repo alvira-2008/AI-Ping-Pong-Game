41469eeeda6fe9afd8fe7b85291b445221597b76
alvira-2008 <72538268+alvira-2008@users.noreply.github.com>
2021-04-05 16:08:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -33,24 +33,46 @@ function setup(){
 
   poseNet = ml5.poseNet(video, modelLoaded);
   poseNet.on('pose', gotPoses);
+  }
+  
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
 }
-
-function modelLoaded(){
-  console.log('PoseNet Initialized');
+  
+function gotPoses(results){
+    if(results.length > 0)
+    {
+  
+      rightWristY = results[0].pose.rightWrist.y;
+      rightWristX = results[0].pose.rightWrist.x;
+      scoreRightWrist =  results[0].pose.keypoints[10].score;
+      console.log(scoreRightWrist);
+    }
 }
-
-
+  
+function startGame(){
+    game_status = "start";
+    document.getElementById("status").innerHTML = "Game Is Loading";
+}
+  
 function draw(){
-
- background(0); 
-
- fill("black");
- stroke("black");
- rect(680,0,20,700);
-
- fill("black");
- stroke("black");
- rect(0,0,20,700);
+  
+  background(0); 
+  image(video, 0, 0, 700, 600);
+  
+  fill("black");
+  stroke("black");
+  rect(680,0,20,700);
+  
+  fill("black");
+  stroke("black");
+  rect(0,0,20,700);
+  
+  if(scoreRightWrist > 0.2){
+    fill("red");
+    stroke("red");
+    circle(rightWristX, rightWristY, 30);
+  }
  
 
  paddleInCanvas();
